553c691f9b9870a0adf76cf7b7f71546d2f37ad4
randyc <rccjoel@gmail.com>
2021-12-12 18:27:54 -0500

    Las formulas ahora son universales

--- geometric.js
+++ geometric.js
@@ -1,57 +1,32 @@
 // Square code
-console.group("Squares")
-const side = 5
-console.log("Los lados del cuadrado miden (cm): " + side)
+function sperimeter(side) {
+    return side * 4
+}
 
-const perimeter = side * 4
-console.log("El perimetro del cuadrado es (cm): " + perimeter)
-
-const area = side * side
-console.log("El area del cuadrado es (cm2): " + area)
-console.groupEnd()
+function sarea(side) {
+    return side * side 
+}
 
 // Triangle code
-console.group("Traingles")
-const side1 = 6
-const side2 = 6
-const base = 4
-const height = 5.5
-console.log("Los lados del tringulo en cm son " 
-    + side1 
-    + ", " 
-    + side2 
-    + ", " 
-    + base
-)
-console.log("La altura del triángulo en cm es " + height)
+function tperimeter(side1, side2, base) {
+    return side1 + side2 + base
+}
 
-const tperimeter = side1 + side2 + base
-console.log("El perimetro del triángulo en cm es " 
-+ tperimeter)
-
-const tarea = (base * height) / 2
-console.log("El area del triángulo en cm2 es "
-+ tarea) 
-console.groupEnd()
+function tarea(base, height) {
+    return (base * height) / 2 
+}
 
 // Circle code
-console.group("Circles")
-
-const radius = 4
-console.log("El radio del círculo en cm es: " + radius )
-
-const diameter = radius * 2
-console.log("El diametro del círculo en cm es: " + diameter )
+function cdiameter(radius) {
+    return radius * 2
+}
 
 const pi = Math.PI
-console.log("La constante Pi en cm es: " + pi )
+function cperimeter(radius) {
+    const diameter = cdiameter(radius)
+    return diameter * pi 
+}
 
-const cperimeter = diameter * pi
-console.log("La circunferencia del círculo en cm es: " 
-+ cperimeter )
-
-const carea = (radius * radius) * pi
-console.log("El area del círculo en cm2 es: " + carea )
-
-console.groupEnd()
-
+function carea(radius) {
+    return pi * (radius * radius)
+}
