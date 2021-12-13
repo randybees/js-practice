7046efc98a42493057389675ed2b76b1b9a18934
randyc <rccjoel@gmail.com>
2021-12-12 21:12:03 -0500

    JS ya interactua con HTML

--- geometric.js
+++ geometric.js
@@ -9,7 +9,7 @@ function sarea(side) {
 
 // Triangle code
 function tperimeter(side1, side2, base) {
-    return side1 + side2 + base
+    return (side1 * 1) + (side2 * 1) + (base * 1)
 }
 
 function tarea(base, height) {
@@ -29,4 +29,55 @@ function cperimeter(radius) {
 
 function carea(radius) {
     return pi * (radius * radius)
+}
+
+//JavaScript interaction for squares
+function squarePerimeter() {
+    const input = document.getElementById("squareInput")
+    const value = input.value
+    const perimeter = sperimeter(value)
+    alert(perimeter)
+}
+
+function squareArea() {
+    const input = document.getElementById("squareInput")
+    const value = input.value
+    const area = sarea(value)
+    alert(area)
+}
+
+//JavaScript interaction for triangles
+function trianglePerimeter() {
+    const input1 = document.getElementById("side1")
+    const first = input1.value
+    const input2 = document.getElementById("side2")
+    const second = input2.value
+    const input3 = document.getElementById("base")
+    const third = input3.value
+    const perimetert = tperimeter(first, second, third)
+    alert(perimetert)
+}
+
+function triangleArea() {
+    const input1 = document.getElementById("base")
+    const first = input1.value
+    const input2 = document.getElementById("height")
+    const second = input2.value
+    const areat = tarea(first, second)
+    alert(areat)
+}
+
+//JavaScript interaction for circles
+function circlePerimeter () {
+    const input = document.getElementById("circleInput")
+    const value = input.value
+    const circumference = cperimeter(value)
+    alert(circumference)
+}
+
+function circleArea () {
+    const input = document.getElementById("circleInput")
+    const value = input.value
+    const areac = carea(value)
+    alert(areac)
 }
